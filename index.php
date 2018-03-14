59fb841465681bc9a8772998c5f5ab5391cc0df9
Maria <maryika93@mail.ru>
2018-03-14 22:18:50 +0600

    sql

--- index.php
+++ index.php
@@ -2,30 +2,6 @@
 
 $connect = mysqli_connect("localhost", "mtipikina", "neto1539", "global");
 $connect->set_charset("utf8");
-$res = mysqli_query($connect,"select * from books");
-
-if (!empty($_POST)) {
-    $ISBN = $_POST['ISBN'];
-    $name = $_POST['name'];
-    $author = $_POST['author'];
-    $resISBN = mysqli_query($connect,"select * from books where isbn like %$ISBN%");
-    while ($dataISBN = mysqli_fetch_array($resISBN)) {
-        echo '<pre>';
-        print_r($dataISBN);
-    }
-
-    $resname = mysqli_query($connect,"select * from books where name like %$name%");
-    while ($dataname = mysqli_fetch_array($resname)) {
-        echo '<pre>';
-        print_r($dataname);
-    }
-
-    $resauthor = mysqli_query($connect,"select * from books where author like %$author%");
-    while ($dataauthor = mysqli_fetch_array($resauthor)) {
-        echo '<pre>';
-        print_r($dataauthor);
-    }
-}
 ?>
 
 <html>
@@ -42,23 +18,65 @@ if (!empty($_POST)) {
         <td align="center"> isbn </td>
         <td align="center"> Жанр </td>
     </tr>
-    <?php while ($data = mysqli_fetch_array($res)){ ?>
-        <tr>
-            <td align="center"><?php echo $data['name']?></td>
-            <td align="center"><?php echo $data['author']?></td>
-            <td align="center"><?php echo $data['year']?></td>
-            <td align="center"><?php echo $data['isbn']?></td>
-            <td align="center"><?php echo $data['genre']?></td>
-        </tr>
-    <?php } ?>
+    <?php if (empty($_POST)) {
+        $res = mysqli_query($connect,"select * from books");
+        while ($data = mysqli_fetch_array($res)) { ?>
+            <tr>
+                <td align="center"><?php echo $data['name'] ?></td>
+                <td align="center"><?php echo $data['author'] ?></td>
+                <td align="center"><?php echo $data['year'] ?></td>
+                <td align="center"><?php echo $data['isbn'] ?></td>
+                <td align="center"><?php echo $data['genre'] ?></td>
+            </tr>
+        <?php }
+    }?>
+    <?php if (!empty($_POST['isbn'])) {
+        $isbn = $_POST['isbn'];
+        $resisbn = mysqli_query($connect,"select * from books where isbn like '%$isbn%'");
+        while ($data = mysqli_fetch_array($resisbn)) { ?>
+            <tr>
+                <td align="center"><?php echo $data['name'] ?></td>
+                <td align="center"><?php echo $data['author'] ?></td>
+                <td align="center"><?php echo $data['year'] ?></td>
+                <td align="center"><?php echo $data['isbn'] ?></td>
+                <td align="center"><?php echo $data['genre'] ?></td>
+            </tr>
+        <?php }
+    }?>
+    <?php if (!empty($_POST['name'])) {
+        $name = $_POST['name'];
+        $resname = mysqli_query($connect,"select * from books where name like '%$name%'");
+        while ($data = mysqli_fetch_array($resname)) { ?>
+            <tr>
+                <td align="center"><?php echo $data['name'] ?></td>
+                <td align="center"><?php echo $data['author'] ?></td>
+                <td align="center"><?php echo $data['year'] ?></td>
+                <td align="center"><?php echo $data['isbn'] ?></td>
+                <td align="center"><?php echo $data['genre'] ?></td>
+            </tr>
+        <?php }
+    }?>
+    <?php if (!empty($_POST['author'])) {
+        $author = $_POST['author'];
+        $resauthor = mysqli_query($connect,"select * from books where author like '%$author%'");
+        while ($data = mysqli_fetch_array($resauthor)) { ?>
+            <tr>
+                <td align="center"><?php echo $data['name'] ?></td>
+                <td align="center"><?php echo $data['author'] ?></td>
+                <td align="center"><?php echo $data['year'] ?></td>
+                <td align="center"><?php echo $data['isbn'] ?></td>
+                <td align="center"><?php echo $data['genre'] ?></td>
+            </tr>
+        <?php }
+    }?>
 </table>
 </body>
 </html>
 
 
 <form method="post" action="" enctype="multipart/form-data">
-    <label>Фильтр по ISBN</label>
-    <input type="text" placeholder="ISBN" name="ISBN"><br/>
+    <label>Фильтр по isbn</label>
+    <input type="text" placeholder="isbn" name="isbn"><br/>
     <label>Фильтр по названию</label>
     <input type="text" placeholder="name" name="name"><br/>
     <label>Фильтр по автору</label>
